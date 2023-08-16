181fcce0609e3f4943cec88778772bf866352a14
jeonggram23 <jeonggram23@gmail.com>
2023-08-16 14:39:44 +0900

    두번째 커밋

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def add_func(n1, n2):
     return n1+n2
 
+
+
+## 전역 변수부
 num1, num2 = 100, 200
 result = 0
 
+## 메인 코드부
 result = add_func(num1, num2)
 print(num1, '+', num2, '=', result)
+
+a = add_func(4,4)
+print(a)
